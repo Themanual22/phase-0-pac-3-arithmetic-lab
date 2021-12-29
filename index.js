2ec22353c7b1ee5bb1a7dd3264c944c2da304b5f
Themanual22 <djamosry22@gmail.com>
2021-12-29 15:55:08 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+} 
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+function increment(n) {
+  return ++ (n)
+}
+function decrement(n) {
+  return -- (n)
+}
+function makeInt (n) {
+  var result=parseInt (n, 10)
+  return result
+}
+function preserveDecimal (n) {
+  var result =parseFloat (n)
+  return result
+
+}
